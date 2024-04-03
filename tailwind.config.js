125c35372196be69dc38d5a540204d3cb3280159
Amir Rahemi <amirrahemi01@gmail.com>
2024-04-03 21:32:48 +0330

    config react.js app

---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'slate': {
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155',
+        800: '#1e293b',
+        900: '#0f172a',
+        950: '#020617',
+        960: '#090909',
+        965: '#111111',
+      },
+      'gray': {
+        500: '#6b7280',
+        600: '#4b5563',
+        800: '#021500',
+      },
+      'green': {
+        100: '#58BB71',
+        700: '#0B2A13',
+      },
+      'red': {
+        100: '#E92C55',
+        200: '#E93131',
+        250: '#ED6464',
+        600: '#350E0C',
+      },
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem',
+      '10xl': '9.5rem',
+      '11xl': '12rem',
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+}
