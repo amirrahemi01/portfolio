b1b96356dd1501ce834f21551718783e4a20487b
Amir Rahemi <amirrahemi01@gmail.com>
2024-04-03 21:40:47 +0330

    modified & deleted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState, useTransition } from 'react';
 import './App.css';
 
+import sideview from "./assets/image/sideview.webp"
+import { useTranslation } from 'react-i18next';
+// import i18next from 'i18next';
+import Navbar from './components/Navbar';
+
+
 function App() {
+  const [t, i18n] = useTranslation("global");
+
+  const handleChangeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    localStorage.setItem('language', lang);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black h-screen text-white">
+      <Navbar />
+      <div className="vanta-cloud-animation"></div>
+      <div className="flex justify-center">
+      <a href="#" className="mousescroll bg-red-100"></a>
+        <h3>{t("header.cancel")}</h3>
+        <button onClick={() => handleChangeLanguage("en")}>EN</button>
+        <button onClick={() => handleChangeLanguage("fa")}>FA</button>
+        <img src={sideview} width={"200px"} />
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Multi-Lingual
+
+import translation_en from "./assets/locale/en/translation.json"
+import translation_fa from "./assets/locale/fa/translation.json"
+
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: {escapeValue: false},
+  lng: localStorage. getItem("language") || 'en',
+  resources: {
+    en: {
+      global: translation_en,
+    },
+    fa: {
+      global: translation_fa,
+    },
+  },
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
+    <I18nextProvider i18n={i18next}>
     <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
