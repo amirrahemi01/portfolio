b8e043876e3c2ed64c8e749da03b7998ae2399b5
Amir Rahemi <amirrahemi01@gmail.com>
2024-04-04 00:29:50 +0330

    target _blank for href

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 import { FaGithub, FaLinkedin, FaMoon, FaXTwitter } from "react-icons/fa6";
 import { IoLogoAngular } from "react-icons/io";
+import { IoLanguage } from "react-icons/io5";
 
 type Props = {}
 
@@ -28,12 +29,15 @@ function Navbar({ }: Props) {
           </li>
         </ul>
         <div className="flex flex-col items-center justify-center">
-          <a href="https://twitter.com/madeby_amir" className="flex items-center justify-center p-0 w-12 h-12 text-white text-3xl"><FaXTwitter /></a>
-          <a href="https://github.com/amirrahemi01" className="flex items-center justify-center p-0 w-12 h-12 text-white text-3xl"><FaGithub /></a>
-          <a href="https://linkedin.com/in/amirrahemi" className="flex items-center justify-center p-0 w-12 h-12 text-white text-3xl"><FaLinkedin /></a>
+          <a href="https://twitter.com/madeby_amir" target="_blank" className="flex items-center justify-center p-0 w-12 h-12 text-white text-3xl"><FaXTwitter /></a>
+          <a href="https://github.com/amirrahemi01" target="_blank" className="flex items-center justify-center p-0 w-12 h-12 text-white text-3xl"><FaGithub /></a>
+          <a href="https://linkedin.com/in/amirrahemi" target="_blank" className="flex items-center justify-center p-0 w-12 h-12 text-white text-3xl"><FaLinkedin /></a>
         </div>
       </nav>
-      <button className="toggle"><FaMoon /></button>
+      <div className="toggle">
+        <button><FaMoon /></button>
+        <button><IoLanguage /></button>
+      </div>
     </header>
   )
 }
