116e51a8bbd4a77fa1b641f1c759967338a6f6ff
amirrahemi01 <amirrahemi01@gmail.com>
2024-04-07 16:23:38 +0330

    update skills content

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 type Props = {}
 
-function Skills({}: Props) {
+function Skills({ }: Props) {
   return (
-    <div>Skills</div>
+    <div>
+      <div className="b">
+        <h3>my skills</h3>
+        <p>
+          Lorem ipsum dolor sit amet
+          consectetur adipisicing elit.
+          Amet, aut quibusdam delectus
+          beatae earum alias eos aspernatur
+          pariatur nemo iste possimus blanditiis
+          officia quidem reprehenderit qui quis
+          commodi nam voluptates!
+        </p>
+        <div className="c">
+          <b>HTML</b>
+          <b>CSS</b>
+          <b>TAILWIND.CSS</b>
+          <b>TYPESCRIPT</b>
+          <b>REACT.JS</b>
+          <b>NEXT.JS</b>
+        </div>
+      </div>
+    </div>
   )
 }
 
