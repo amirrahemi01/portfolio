a5a31706fdcf9f2ef7f7b53fc83a752becb2b413
amirrahemi01 <amirrahemi01@gmail.com>
2024-04-07 16:24:09 +0330

    update contenx of component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 type Props = {}
 
-function About({}: Props) {
+function About({ }: Props) {
   return (
-    <div>About</div>
+    <div>
+      <div className="a">
+        <h2>I AM A REACT.JS DEVELOPER</h2>
+        <p>
+          Lorem ipsum dolor sit amet
+          consectetur adipisicing elit.
+          Amet, aut quibusdam delectus
+          beatae earum alias eos aspernatur
+          pariatur nemo iste possimus blanditiis
+          officia quidem reprehenderit qui quis
+          commodi nam voluptates!
+        </p>
+        <img src="amir" alt="" />
+        <button>Download CV</button>
+      </div>
+      
+      <div className="d">
+        <h1>my interests</h1>
+        <div className="e">
+          <div className="f">
+            <b>x</b>
+          </div>
+          <div className="f">
+            <b>y</b>
+          </div>
+          <div className="f">
+            <b>z</b>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
