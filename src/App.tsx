7fa42844d81c2d28da404e46f39b0af3133f3b7b
amirrahemi01 <amirrahemi01@gmail.com>
2024-04-10 13:29:20 +0330

    before GYM :)

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import IMG from "./assets/image/cover.jpg"
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import SocialMedia from './components/SocialMedia';
 import ContactUs from './components/ContactUs';
 
 
@@ -86,7 +85,7 @@ function App() {
 
         <section className="h-screen flex items-center justify-center flex-col text-white" id="a">
 
-          <SocialMedia />
+          
           <ContactUs />
 
         </section>
--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,7 +1,9 @@
 import React, { useRef, FormEvent } from 'react';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { FaFacebookF, FaGithub, FaInbox, FaInstagram, FaLinkedinIn, FaLocationArrow, FaSquareXTwitter, FaX } from 'react-icons/fa6';
+import { MdOutlineAlternateEmail } from 'react-icons/md';
 
-interface ContactProps {}
+interface ContactProps { }
 
 const ContactUs: React.FC<ContactProps> = () => {
   const form = useRef<HTMLFormElement>(null);
@@ -26,15 +28,34 @@ const ContactUs: React.FC<ContactProps> = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className="flex flex-col">
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="inline-flex items-center gap-4 w-full">
+
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col text-blue-100 float-left">
+        <h2 className="text-4xl">Just Say Hello!</h2>
+        <div className="inline-flex gap-4">
+          <input className="bg-slate-655 focus:outline-none rounded-xl p-5 my-4 w-1/2" placeholder="Name" type="text" name="user_name" />
+
+          <input className="bg-slate-655 focus:outline-none rounded-xl p-5 my-4 w-1/2" placeholder="Email" type="email" name="user_email" />
+        </div>
+
+        <textarea className="bg-slate-655 focus:outline-none rounded-xl p-5 my-4 resize-none" placeholder="Message" name="message" />
+        <input className="bg-blue-100 text-black px-4 py-2 w-28 rounded-xl cursor-pointer" type="submit" value="Send" />
+      </form>
+
+
+      <div className="float-right flex flex-col justify-center items-center w-1/2 p-5 h-full rounded-xl gap-2">
+        <h2 className="text-3xl">More</h2>
+        <br />
+        <div className="flex flex-row justify-center text-5xl">
+          <a href="mailto:amirrahemi01@gmail.com"><MdOutlineAlternateEmail /></a>
+          <a href="https://linkedin.com/in/amirrahemi"><FaLinkedinIn /></a>
+          <a href="https://linkedin.com/"><FaGithub /></a>
+          <a href="https://linkedin.com/madeby_amir"><FaFacebookF /></a>
+          <a href="https://linkedin.com/amirrahemiii"><FaInstagram /></a>
+          <a href="https://linkedin.com/madeby_amir"><FaSquareXTwitter /></a>
+        </div>
+      </div>
+    </div>
   );
 };
 
